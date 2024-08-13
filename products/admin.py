dc11bdd4f2acf80e61520b66bd782318e88cd4d4
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-08-13 15:50:15 +0100

    Pillow installed. Image URL for admin changed to upload. Calender layout changed

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.admin.widgets import AdminDateWidget, AdminTimeWidget
 from .models import Product, Category, CalendarEvent
+
+# Custom form to customize date and time widgets
+class CalendarEventForm(forms.ModelForm):
+    class Meta:
+        model = CalendarEvent
+        fields = ['product', 'start_time', 'end_time', 'location']
+        widgets = {
+            'start_time': forms.SplitDateTimeWidget(
+                date_attrs={'type': 'date'},
+                time_attrs={'type': 'time'}
+            ),
+            'end_time': forms.TimeInput(attrs={'type': 'time'}),
+        }
 
 class CalendarEventInline(admin.TabularInline):
     model = CalendarEvent
+    form = CalendarEventForm
     extra = 1
 
 class ProductAdmin(admin.ModelAdmin):
@@ -12,4 +28,3 @@ class ProductAdmin(admin.ModelAdmin):
 # Registered models with Django admin
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
-admin.site.register(CalendarEvent)
