6523f1680244c6cd82de8fa1322dd86b83a46d0b
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-08-14 22:51:52 +0100

    Date/Time choice added to workshop list. Layout improved, added bootstrap cards

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='workshop_list'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,21 @@
 # products/views.py
 from django.shortcuts import render
-from .models import Product
+from .models import Product, CalendarEvent
+from django.shortcuts import get_object_or_404, redirect
+
+
 
 def product_list(request):
-    products = Product.objects.all()
+    products = Product.objects.prefetch_related('events').all()
     return render(request, 'products/workshop_list.html', {'products': products})
+
+
+def add_to_cart(request, product_id):
+    if request.method == "POST":
+        event_id = request.POST.get('event')
+        event = get_object_or_404(CalendarEvent, id=event_id)
+        # Add logic to add the event to the user's cart
+        # e.g., request.session['cart'].append(event)
+        return redirect('cart_view')  # Redirect to the cart page or another appropriate page
+
+    return redirect('product_list')  # Redirect to product list if not a POST request
