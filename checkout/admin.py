34d511956b0c8734ee1dbb778e9294083579778a
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-08-18 17:01:51 +0100

    admin, forms and signals added to checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+class OrderLineItemInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderLineItemInline]
+
+    readonly_fields = ('order_number', 'date_of_birth', 'grand_total',)
+
+    # fieldsets should define fields within a section (can be nested)
+    fields = ('order_number', 'full_name', 'email', 'phone_number',
+              'postcode', 'street_address1', 'street_address2')
+
+    list_display = ('order_number', 'date_of_birth', 'full_name')
+
+    ordering = ('-date_of_birth',)
+
+admin.site.register(Order, OrderAdmin)
