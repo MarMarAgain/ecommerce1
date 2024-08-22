af81b0302528dd926a11cd40a116fbf69609a0a1
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-08-22 16:27:25 +0100

    Fixed allauth package bug issues

--- PP5_ecommerce/settings.py
+++ PP5_ecommerce/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -28,9 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,7 +39,7 @@ INSTALLED_APPS = [
     'home',
     'crispy_forms',
 
-    # Allauth apps...
+    # Allauth apps
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -53,10 +50,10 @@ INSTALLED_APPS = [
     "bookings.apps.BookingsConfig",
     "bag.apps.BagConfig",
     "checkout.apps.CheckoutConfig",
-    "profiles.apps.ProfilesConfig"
+    "profiles.apps.ProfilesConfig",
+    # Uncomment to enable other providers
     # 'allauth.socialaccount.providers.facebook',
     # 'allauth.socialaccount.providers.twitter',
-
 ]
 
 MIDDLEWARE = [
@@ -85,7 +82,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  # required by allauth
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
@@ -99,11 +96,9 @@ TEMPLATES = [
     },
 ]
 
-
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
-
     # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
 )
@@ -112,10 +107,8 @@ SITE_ID = 1
 
 WSGI_APPLICATION = 'PP5_ecommerce.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -123,10 +116,8 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -143,49 +134,36 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # For sending confirmation emails
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development, it prints emails to the console. Change this in Heroku.
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development, it prints emails to the console. Change this in production.
 
 # Allauth settings
-ACCOUNT_AUTHENTICATION_METHOD = 'username_email'  #email & username
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'  # email & username
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  # Or 'optional' or 'none'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = 'accounts/login'
-LOGIN_REDIRECT_URL = '/success' #temporary
-
+LOGIN_REDIRECT_URL = '/success'  # Update as needed
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 # Stripe
 STRIPE_CURRENCY = 'eur'
